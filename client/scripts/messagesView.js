b449841bf7a069972e5316b80c4cf510a404493a
Miriam Wipf <miriam.wipf1@gmail.com>
2020-11-16 20:28:34 -0800

    Passed "Creating" tests for Ajax Behavior (Chatterbox)

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -6,6 +6,17 @@ var MessagesView = {
   },
 
   render: function() {
+  },
+
+  renderMessage: function(message) {
+    var htmlMessage = MessageView.render(
+
+      // var message = {
+      //   username: 'shawndrost',
+      //   text: 'trololo',
+      //   roomname: '4chan'
+      // };
+    )
   }
 
 };
